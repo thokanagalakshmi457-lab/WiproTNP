a787a467797e020b12ef7c1b3fe29d983358b9dc
thokanagalakshmi457-lab <thokanagalakshmi457@gmail.com>
2026-07-15 22:39:48 +0530

    Added logicBuildingM1 looping programs

---
+++ src/logicBuildingM1/BackwardsNumbers.java
@@ -0,0 +1,11 @@
+package logicBuildingM1;
+
+public class BackwardsNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 100; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
